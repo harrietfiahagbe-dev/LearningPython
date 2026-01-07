f77a90bd415ca35155366fea3d9e64d8984e570b
harrietfiahagbe-dev <harrietfiahagbe@gmail.com>
2026-01-07 15:12:41 +0000

    new onee

--- Learning1.0.py
+++ Learning1.0.py
@@ -1,1 +1,24 @@
+#INTRODUCTION
 print("Hello, Welcome to Learning Python!")
+print(3+5) #ADDITION
+print(8+10) #SUBTRACTION
+print(10-7) #SUBTRACTION
+print(9*8) #MULTIPLICATION
+print(3/2)
+print(3**2) #Exponential value
+print(10%3)#Modulus (Gives the remainder when smth is dividing smth)
+print(12//5) #Floor division sign, removes the decimal attached to it
+
+
+#CHECKING DATA TYPES
+print(type(10)) #int
+print(type(3.4)) #float
+print(type(3 +4j)) #complex number
+print(type("Yayra")) #string
+print(type([1,2,2])) # list: they are ordered, because of the index and they are mutable can be changed easily, can have other datat types, including lists,int etc. use sqaure braket for list
+print(type({"Name" : "Yayra"})) #dictionary, they are unordered pairs, can be selected only from keys and values, no index, the keys have to be unique, we have to use curly bracket for dictionaries, they are mutable as well
+print(type({3,7,8.9})) #set, they are unique and mutable, no duplicates in the values, and they are unordered (index? : yes no index that is why they are unordered), also uses curly brackets, can contain all data types excepts from dictionaries and lists
+print(type((1,2,2)))#ordered, and they are immutable, cannot change when it has been placed in the brackets, can hold different dtat type
+
+
+
