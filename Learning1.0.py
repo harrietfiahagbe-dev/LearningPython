662e9133c2ebee92529eda3c2dc7adf7166c5275
harrietfiahagbe-dev <harrietfiahagbe@gmail.com>
2026-01-07 17:40:43 +0000

    done with day2!

--- Learning1.0.py
+++ Learning1.0.py
@@ -1,4 +1,5 @@
 #INTRODUCTION
+'''
 print("Hello, Welcome to Learning Python!")
 print(3+5) #ADDITION
 print(8+10) #SUBTRACTION
@@ -21,4 +22,59 @@ print(type({3,7,8.9})) #set, they are unique and mutable, no duplicates in the v
 print(type((1,2,2)))#ordered, and they are immutable, cannot change when it has been placed in the brackets, can hold different dtat type
 
 
+#VARIABLES AND BUILT IN FUNCTIONS
+#Make use of built in functions without importing or configuring
+first_name = "Yayra"
+country = "Ghana"
+skills = ["java", "python", "geogebra", "vsco"]
+my_info = {"Name" : "Yayra",
+           "Age" : 19,
+           "City" : "Elmina",
+           "Sisters" : "Elorm,Senam"
+           }
+print(len(first_name))
+print("Name:",first_name)
+print("Country:" ,country)
+print("My Info: ", my_info)
 
+#Declaring muitple variables in one line
+name, age, school, grade = "Boven", 19, "Ashesi", "A+"
+print("New Name:" , name )
+print("New Age:" , age)
+print("Uni:" ,school)
+print("Grade" , grade)
+
+name_info = input("What is your name?")
+age_info = input("What is your age?")
+print(name_info)
+print(age_info)
+'''
+
+#Casting
+#int to float
+num_int = 10
+print("Int Num:", num_int)
+num_float = float(num_int)
+print("Float Num:",num_float )
+
+#float to integer
+new_num = 5.67
+int_num = int(new_num)
+print("Conversion:", int_num)
+
+#int to str
+#using num_int
+num_str = str(num_int)
+print("String Number: ", num_str)
+
+#Str to int or float
+num_str = "10.6"
+float_num = float(num_str)
+print("Float Number", float_num)
+new_intone = int(float_num)
+print("Integer", new_intone)# so we can change from string to float, then from float to string, we cant directly change from string to int
+
+#str to list
+name_str = "Yayra"
+list_name = list(name_str)
+print("List in Names", list_name)
